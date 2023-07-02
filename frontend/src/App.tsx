ab95afffb7ca3ed89cb2ba9359232f6cc9fa010f
oliver1384 <oliver1384@83nd7wpw.home>
2023-07-02 16:59:22 +0100

    Implement styles to the body page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
-import "./App.css";
+import { PRIMARY_COLORS } from "./colors";
 import { Biography } from "./components/Biography";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  page: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxWidth: '1240px',
+    paddingLeft: '1rem',
+    paddingRight: '1rem',
+    backgroundColor: PRIMARY_COLORS.element
+  },
+});
+
 
 export const App = () => {
+  const classes = useStyles()
   return (
     <>
       <header></header>
-      <body>
+      <body className={classes.page}>
         <Biography />
       </body>
       <footer></footer>
--- frontend/src/colors.ts
+++ frontend/src/colors.ts
@@ -1,8 +1,8 @@
 export const PRIMARY_COLORS = {
   component: "#2C3333",
   section: "#395B64",
-  pageBackground: "#E7F6F2",
-  element: "#A5C9CA",
+  element: "#E7F6F2",
+  pageBackground: "#A5C9CA",
 };
 
 export const TEXT_COLORS = {
