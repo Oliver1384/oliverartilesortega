cfa09b886714c782cfda3f5b1b76d6a50807eff8
oliver1384 <oliver1384@83nd7wpw.home>
2023-07-22 11:48:13 +0100

    Create a HamburgerMenu component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,27 @@
 import { PRIMARY_COLORS } from "./colors";
 import { Biography } from "./components/Biography";
 import { makeStyles } from "@mui/styles";
+import { HamburgerMenu } from "./components/HamburgerMenu";
 
 const useStyles = makeStyles({
   page: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    maxWidth: '1240px',
-    paddingLeft: '1rem',
-    paddingRight: '1rem',
-    backgroundColor: PRIMARY_COLORS.element
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    maxWidth: "1240px",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+    backgroundColor: PRIMARY_COLORS.element,
   },
 });
 
-
 export const App = () => {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <>
-      <header></header>
+      <header>
+        <HamburgerMenu />
+      </header>
       <body className={classes.page}>
         <Biography />
       </body>
