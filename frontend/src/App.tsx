5c509fdec67042f49b6c4fc42fa6dd35a52c13f8
oliver1384 <oliver1384@83nd7wpw.home>
2023-07-23 13:31:02 +0100

    Add eslint and prettier configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,22 @@
-import { PRIMARY_COLORS } from "./colors";
-import { Biography } from "./components/Biography";
-import { makeStyles } from "@mui/styles";
-import { HamburgerMenu } from "./components/HamburgerMenu";
+import { PRIMARY_COLORS } from './colors'
+import { Biography } from './components/Biography'
+import { makeStyles } from '@mui/styles'
+import { HamburgerMenu } from './components/HamburgerMenu'
 
 const useStyles = makeStyles({
   page: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    maxWidth: "1240px",
-    paddingLeft: "1rem",
-    paddingRight: "1rem",
-    backgroundColor: PRIMARY_COLORS.element,
-  },
-});
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    maxWidth: '1240px',
+    paddingLeft: '1rem',
+    paddingRight: '1rem',
+    backgroundColor: PRIMARY_COLORS.element
+  }
+})
 
 export const App = () => {
-  const classes = useStyles();
+  const classes = useStyles()
   return (
     <>
       <header>
@@ -27,5 +27,5 @@ export const App = () => {
       </body>
       <footer></footer>
     </>
-  );
-};
+  )
+}
--- frontend/src/components/HamburgerMenu.tsx
+++ frontend/src/components/HamburgerMenu.tsx
@@ -1,53 +1,51 @@
-import { AppBar, Drawer, IconButton, Toolbar, Box } from "@mui/material";
-import { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
-import { makeStyles } from "@mui/styles";
+import { AppBar, Drawer, IconButton, Toolbar, Box } from '@mui/material'
+import { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
+import { makeStyles } from '@mui/styles'
 
 const useStyles = makeStyles({
   navbar: {
-    justifyContent: "space-around",
-    "@media(max-width:480px)": {
-      display: "none",
-    },
+    justifyContent: 'space-around',
+    '@media(max-width:480px)': {
+      display: 'none'
+    }
   },
   hamburgerIcon: {
-    display: "block",
-    marginRight: "2rem",
-    "@media(min-width:480px)": {
-      display: "none",
+    display: 'block',
+    marginRight: '2rem',
+    '@media(min-width:480px)': {
+      display: 'none'
     },
-    backgroundColor:"black"
-  },
-});
+    backgroundColor: 'black'
+  }
+})
 
 export const HamburgerMenu = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
-  const classes = useStyles();
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
+  const classes = useStyles()
 
   return (
     <AppBar position="static">
       <Toolbar>
-        {/* hamburger icon shows the drawer on click */}
         <IconButton
           edge="start"
           onClick={() => setIsDrawerOpen(true)}
-          className={classes.hamburgerIcon}
-        >
+          className={classes.hamburgerIcon}>
           <MenuIcon />
         </IconButton>
         <div className={`${classes.navbar}`}>
           <button>Biografía</button>
         </div>
         <Drawer
-          anchor="right" //from which side the drawer slides in
+          anchor="right"
           variant="temporary" //if and how easily the drawer can be closed
-          open={isDrawerOpen} //if open is true, drawer is shown
-          onClose={() => setIsDrawerOpen(false)} //function that is called when the drawer should close
+          open={isDrawerOpen}
+          onClose={() => setIsDrawerOpen(false)}
           //onOpen={() => setIsDrawerOpen(true)} function that is called when the drawer should open
         >
           <Box>{/* The inside of the drawer */}</Box>
         </Drawer>
       </Toolbar>
     </AppBar>
-  );
-};
+  )
+}
